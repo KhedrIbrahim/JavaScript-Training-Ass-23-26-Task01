8709e285d12ebb3b58c6b03c42cb692b2bb7f88c
Khedr Ibrahim <108041341+KhedrIbrahim@users.noreply.github.com>
2023-01-04 06:41:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+console.log(1e5); // 100000
+console.log(10 * 10000); // 100000
+console.log(10 ** 5); // 100000
+console.log(100000.0); // 100000
+console.log(Math.round(100000.2)); // 100000
+console.log(parseInt(100000.5555)); // 100000
+console.log(Math.trunc(100000.5)); // 100000
+console.log(Math.floor(100000.4)); // 100000
